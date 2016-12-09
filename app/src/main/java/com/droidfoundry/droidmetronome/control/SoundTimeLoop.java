b7c53cd03f7fd4e9c45bce982093e5ec370155c5
RenanOfugi <renangyn2010@hotmail.com>
2016-12-09 11:42:05 -0300

    SoundTimeLoop arrumado

--- app/src/main/java/com/droidfoundry/droidmetronome/control/SoundTimeLoop.java
+++ app/src/main/java/com/droidfoundry/droidmetronome/control/SoundTimeLoop.java
@@ -3,13 +3,14 @@ package com.droidfoundry.droidmetronome.control;
 import android.os.CountDownTimer;
 import android.os.Looper;
 
+import com.droidfoundry.droidmetronome.exceptions.SoundException;
 import com.droidfoundry.droidmetronome.model.FiguraRitmica;
 import com.droidfoundry.droidmetronome.model.TemplateSound;
 
 /**
  * Created by pedro on 12/05/15.
  */
-public class SoundTimeLoop extends CountDownTimer{
+public class SoundTimeLoop extends CountDownTimer {
 
     private TemplateSound templateSound;
     private FiguraRitmica figuraRitmica;
@@ -21,8 +22,9 @@ public class SoundTimeLoop extends CountDownTimer{
 
     /**
      * Construtor do timer
-     * @param templateSound - som a ser tocado
-     * @param millisInFuture - tempo máximo
+     *
+     * @param templateSound     - som a ser tocado
+     * @param millisInFuture    - tempo máximo
      * @param countDownInterval - tempo entre toques
      */
     public SoundTimeLoop(TemplateSound templateSound, long millisInFuture, long countDownInterval) {
@@ -38,28 +40,29 @@ public class SoundTimeLoop extends CountDownTimer{
 
     /**
      * Método para cada tick
+     *
      * @param l
      */
     @Override
-    public void onTick(long l){
+    public void onTick(long l) {
         try {
             // Ativando luz
             HardwareActions.getInstance().activeLighting();
 
 
-            if((figuraRitmica.getValue() >1 ) && (!this.starting)){
+            if ((figuraRitmica.getValue() > 1) && (!this.starting)) {
                 batidasMaximo = batidasMaximo * this.figuraRitmica.getValue() - (this.figuraRitmica.getValue() - 1);
                 this.starting = true;
             }
 
-            if(batidasAtual < batidasMaximo) {
+            if (batidasAtual < batidasMaximo) {
 
                 // Modo vibratório
                 HardwareActions.getInstance().activeVibration();
                 templateSound.playSoundAlto();
 
                 batidasAtual++;
-            }else{
+            } else {
 
                 // Modo vibratório
                 HardwareActions.getInstance().activeVibration();
@@ -72,8 +75,8 @@ public class SoundTimeLoop extends CountDownTimer{
             HardwareActions.getInstance().desactiveLighting();
 
 
-        }catch(RuntimeException erro) {
-
+        } catch (RuntimeException erro) {
+            throw new SoundException("Erro na execução do tick");
         } finally {
             // Desativar flash
             HardwareActions.getInstance().desactiveLighting();
@@ -91,17 +94,19 @@ public class SoundTimeLoop extends CountDownTimer{
 
     /**
      * Método que define uma figura ritmica para o contador
+     *
      * @param figuraRitmica
      */
-    public void setFiguraRitmica(FiguraRitmica figuraRitmica){
+    public void setFiguraRitmica(FiguraRitmica figuraRitmica) {
         this.figuraRitmica = figuraRitmica;
     }
 
     /**
      * Método para adicionar as batidas máximo
+     *
      * @param batidasMaximo
      */
-    public void setBatidasMaximo(int batidasMaximo){
+    public void setBatidasMaximo(int batidasMaximo) {
         this.batidasMaximo = batidasMaximo;
     }
 
--- app/src/main/java/com/droidfoundry/droidmetronome/exceptions/SoundException.java
+++ app/src/main/java/com/droidfoundry/droidmetronome/exceptions/SoundException.java
@@ -1,8 +1,8 @@
 package com.droidfoundry.droidmetronome.exceptions;
 
-/**
- * Created by cintia on 09/12/16.
- */
+public class SoundException extends RuntimeException {
 
-public class SoundException {
+    public SoundException(final String mensagem) {
+        super(mensagem);
+    }
 }
